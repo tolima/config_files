aa673cb8b59242326031b9d93cb220a406be8f85
Yoann Ricordel <leyaude@gmail.com>
2012-10-15 17:01:32 +0200

    REmove useless euro2sek and stuff useless with crunchbang

--- bin/sek_resources.py
+++
@@ -1,9 +0,0 @@
-one_euro_in_sek = 8.32
-one_sek_in_euro = 1./one_euro_in_sek
-
-
-def sek_to_euro(price_in_sek):
-    return price_in_sek * one_sek_in_euro
-
-def euro_to_sek(price_in_euro):
-    return price_in_euro * one_euro_in_sek
